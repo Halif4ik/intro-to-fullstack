377a25bdc4af61ae4a70f578ad7a23636f3f1a30
Windows10 <you@example.com>
2020-07-13 17:36:04 +0300

    add adge case in 3 warmup

--- ps2-Javascript_RegExp/warm_up/main.js
+++ ps2-Javascript_RegExp/warm_up/main.js
@@ -43,10 +43,8 @@ document.getElementById("btn2_1").addEventListener('click',()=>{
     let result =prepeareResult(arrHours[0])+':'+ prepeareResult(arrHours[1])+':'+prepeareResult(arrHours[2])
 
 
-
     document.getElementById('hh:mm:ss').innerText = result;
-
-    }  );
+    } );
 
 document.getElementById("btn2_2").addEventListener('click',()=>{
     let timeInHHMMSS = document.getElementById('timeHhMmSS').value;
@@ -56,6 +54,65 @@ document.getElementById("btn2_2").addEventListener('click',()=>{
 
     return document.getElementById('result2_2').innerText = hhMmSs(parsTime);
     }  );
+
+/*3. User put time in input[type=datetime-local] output instring  2 year(s), 1 month(s),
+3 day(s), 5 hour(s), 10 minute(s), 15 second(s).*/
+document.getElementById("btn3").addEventListener('click',()=>{
+    let arrHours = [];
+    let result;
+    arrHours[0] = document.getElementById('datetimeStart').value;
+    arrHours[1] = document.getElementById('datetimeEnd').value;
+
+    if(!arrHours[0] || !arrHours[1] ) alert('Empty datetime Start or End');
+    if(arrHours[0] > arrHours[1]) arrHours.reverse();
+
+    const dateStart = new Date(arrHours[0]);
+    const dateEnd = new Date(arrHours[1]);
+
+    let year = dateEnd.getFullYear()-dateStart.getFullYear();
+
+    let endMount = dateEnd.getMonth();
+    let startMount = dateStart.getMonth();
+    if (endMount  < startMount){
+        year--;
+        endMount+=12;
+    }
+    let month = endMount - startMount;
+
+    //calculate difference how many days
+    let endDays = dateEnd.getDate();
+    let startDays = dateStart.getDate();
+    let days;
+    if (endDays  < startDays){
+        month--;
+        days  = Math.round((dateEnd - dateStart) / 1000 / 60*60 / 24);
+         } else days= endDays-startDays;
+
+    //calculate difference how many hours
+    let endHour =  dateEnd.getHours();
+    let startHour =  dateStart.getHours();
+    let hours;
+    if (endHour  < startHour){
+        days--;
+        endHour += 24;
+    }
+    hours= endHour-startHour;
+    //calculate difference how many minutes
+    let endMinute =  dateEnd.getMinutes();
+    let startMinute =  dateStart.getMinutes();
+    let minutes;
+    if (endMinute <startMinute){
+        hours--;
+        endMinute += 60;
+    }
+     minutes = endMinute-startMinute;
+
+    const second = dateEnd.getSeconds()-dateStart.getSeconds();
+
+result = `${year} year(s), ${month} month(s), ${days} day(s), ${hours} hour(s), ${minutes} minute(s), ${second} second(s)`
+    document.getElementById('yearMonth').innerText = result;
+});
+
 
 const SECOND_IN_HOUR =3600;
 const SECOND_IN_MINUTE =60;
